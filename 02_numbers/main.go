3e69f3411da587fc06da7a934ad3e37020e5234d
ahmad_ali_moshtaghi <moshtaghi@gmail.com>
2023-06-10 23:06:53 +0330

    numbers

---
+++ 02_numbers/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var x int
+	// var y int
+
+	// x := 2.0
+	// y := 1.0
+
+	x := 2
+	y := 1
+
+	fmt.Printf("x = %v, type of %T\n", x, x)
+	fmt.Printf("y = %v, type of %T\n", y, y)
+
+	var mean float64
+
+	// mean = (x + y) / 2.0
+
+	mean = float64(x+y) / 2.0
+
+	fmt.Printf("mean = %v, type of %T\n", mean, mean)
+}
