56a810cbbc9f45b157c5c97d4cefb12a622e7a9d
ahmad_ali_moshtaghi <moshtaghi@gmail.com>
2023-06-12 10:21:02 +0330

    update

---
+++ 06_array_and_slice/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	names := []string{"ahmad", "ali", "sajjad", "hasan"}
+
+	// length
+	fmt.Println(len(names))
+	fmt.Println("===================================")
+
+	//index
+	fmt.Println(names[1])
+	fmt.Println("===================================")
+
+	//slice
+	fmt.Println(names[:2])
+	fmt.Println("===================================")
+
+	for i := 0; i < len(names); i++ {
+		fmt.Println(names[i])
+	}
+	fmt.Println("===================================")
+
+	// single value range(to show only index)
+	for i := range names {
+		fmt.Println(i)
+	}
+	fmt.Println("===================================")
+
+	// double value range (to show index and value)
+	for i, name := range names {
+		fmt.Println(i, name)
+	}
+	fmt.Println("===================================")
+
+	// double value range (to show index and value)
+	for i, name := range names {
+		fmt.Printf("%d - %s\n", i, name)
+	}
+	fmt.Println("===================================")
+
+	// double value range (ignore index)
+	for _, name := range names {
+		fmt.Println(name)
+	}
+	fmt.Println("===================================")
+
+	fmt.Printf("names = %v type(%T)\n", names, names)
+
+}
