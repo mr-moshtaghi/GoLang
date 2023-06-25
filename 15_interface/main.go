a6e037e90e06e43894fa04ec171640a03f13a406
ahmad_ali_moshtaghi <moshtaghi@gmail.com>
2023-06-25 18:27:31 +0330

    update

--- 15_interface/main.go
+++ 15_interface/main.go
@@ -54,3 +54,39 @@ func main() {
 
 	fmt.Println(sc)
 }
+
+
+
+//package main
+//
+//import "fmt"
+//
+//type fake2 struct {
+//	content2 string
+//}
+//
+//func (s *fake2)string2() string {
+//	return s.content2
+//}
+//
+//type fake3 struct {
+//	content3 string
+//}
+//
+//func (s fake3)string3() string {
+//	return s.content3
+//}
+//
+//func printing(val interface{}) {
+//	fmt.Println(val.(test).string3())
+//}
+//
+//type test interface {
+//	string3() string
+//}
+//
+//func main() {
+//	sample := fake3{content3: "dhfdsfhsdkhfdjh"}
+//	printing(sample)
+//}
+
